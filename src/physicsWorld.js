983faf1590860ba54bd8e3a763f3b42b36360cc7
Victor <78041023+victor31657513@users.noreply.github.com>
2025-07-18 15:03:11 +0200

    Increase solver iterations for better collisions (#37)

--- src/physicsWorld.js
+++ src/physicsWorld.js
@@ -3,7 +3,8 @@ import * as CANNON from 'https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/dist/cann
 const world = new CANNON.World();
 world.gravity.set(0, 0, 0);
 world.broadphase = new CANNON.SAPBroadphase(world);
-world.solver.iterations = 10;
+// Increase solver iterations to handle many rider collisions
+world.solver.iterations = 20;
 
 const defaultMaterial = new CANNON.Material('defaultMaterial');
 const contactMaterial = new CANNON.ContactMaterial(defaultMaterial, defaultMaterial, { friction: 0.3, restitution: 0.1 });
