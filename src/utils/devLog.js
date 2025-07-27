70f3c3fa7850fae32f1339e47b9cca5ff3c5c3f9
Victor <78041023+victor31657513@users.noreply.github.com>
2025-07-27 15:59:09 +0200

    refactor overlap resolution for rapier

--- src/utils/devLog.js
+++ src/utils/devLog.js
@@ -1,4 +1,6 @@
-const devMode = new URLSearchParams(window.location.search).has('dev');
+const devMode =
+  typeof window !== 'undefined' &&
+  new URLSearchParams(window.location.search).has('dev');
 
 function devLog(...args) {
   if (devMode) console.log(...args);
